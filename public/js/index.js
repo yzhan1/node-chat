a4e77e69a96b54b3d988ca75bb68897272762851
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-07-07 17:11:09 +0800

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'Christina',
-    text: 'Good how are you?',
-  });
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Peter',
-    text: 'Hey what is going on?',
-    createAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
