451b1fa2f51ad19cacfec43d158644f9be7bed05
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-07-08 11:34:05 +0800

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+function scrollToBottom(){
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function(){
   console.log('Connected to server');
 });
@@ -18,6 +32,7 @@ socket.on('newMessage', function(message){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -30,6 +45,7 @@ socket.on('newLocationMessage', function(message){
   });
   
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e){
